a7325301d4a8bc130d265c21924a8707a792e43c
Yidi <yidi.zhao@thoughtworks.com>
2020-02-29 14:59:29 +0800

    [code refactor]long method

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -13,52 +13,67 @@ class GildedRose {
 
     public void update_quality() {
         for (Item item : items) {
+            updateItem(item);
+        }
+    }
 
-            if (!item.isNameEquals(agedBrie)
-                    && !item.isNameEquals(backstagePasses)) {
-                if (item.quality > 0) {
-                    if (!item.isNameEquals(sulfuras)) {
-                        item.quality = item.quality - 1;
+    private void updateItem(Item item) {
+        updateQuality(item);
+
+        updateSellIn(item);
+
+        updateQualityWhenExpired(item);
+    }
+
+    private void updateQualityWhenExpired(Item item) {
+        if (item.sell_in < 0) {
+            if (!item.isNameEquals(agedBrie)) {
+                if (!item.isNameEquals(backstagePasses)) {
+                    if (item.quality > 0) {
+                        if (!item.isNameEquals(sulfuras)) {
+                            item.quality = item.quality - 1;
+                        }
                     }
+                } else {
+                    item.quality = 0;
                 }
             } else {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
-
-                    if (item.isNameEquals(backstagePasses)) {
-                        if (item.sell_in < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-
-                        if (item.sell_in < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
                 }
             }
+        }
+    }
 
-            if (!item.isNameEquals(sulfuras)) {
-                item.sell_in = item.sell_in - 1;
+    private void updateSellIn(Item item) {
+        if (!item.isNameEquals(sulfuras)) {
+            item.sell_in = item.sell_in - 1;
+        }
+    }
+
+    private void updateQuality(Item item) {
+        if (!item.isNameEquals(agedBrie)
+                && !item.isNameEquals(backstagePasses)) {
+            if (item.quality > 0) {
+                if (!item.isNameEquals(sulfuras)) {
+                    item.quality = item.quality - 1;
+                }
             }
+        } else {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
 
-            if (item.sell_in < 0) {
-                if (!item.isNameEquals(agedBrie)) {
-                    if (!item.isNameEquals(backstagePasses)) {
-                        if (item.quality > 0) {
-                            if (!item.isNameEquals(sulfuras)) {
-                                item.quality = item.quality - 1;
-                            }
+                if (item.isNameEquals(backstagePasses)) {
+                    if (item.sell_in < 11) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
                         }
-                    } else {
-                        item.quality = 0;
                     }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
+
+                    if (item.sell_in < 6) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
                     }
                 }
             }
